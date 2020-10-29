78ab23913d5b6bc3cde5d6e14786128bfb8ef3c3
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-10-29 17:46:43 -0300

    update

--- calculator/src/main/Calculator.js
+++ calculator/src/main/Calculator.js
@@ -6,7 +6,9 @@ import './calculator.css';
 const initialState ={
     displayValue: "0",
     clearDisplay: false,
-    operation: null
+    operation: null,
+    values: [0, 0],
+    current: 0
 }
 
 export default class Calculator extends Component {
@@ -24,7 +26,31 @@ export default class Calculator extends Component {
     };
 
     setOperation(operation) {
-        console.log(operation)
+        if(this.state.current === 0) {
+            this.setState({ operation, current: 1, clearDisplay: true });
+
+        } else {
+            const equals = operation === '=';
+            const currentOperation = this.state.operation;
+            const values = [...this.state.values];
+
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`);
+            } catch (error) {
+                values[0] = this.state.values[0];
+            }
+
+            values[1] = 0;
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+
+        }
     };
 
     addDigit(n) {
@@ -36,7 +62,16 @@ export default class Calculator extends Component {
 
         const currentValue = clearDisplay ? '' : this.state.displayValue;
         const displayValue = currentValue + n;
-        this.setState({ displayValue, clearDisplay: false })
+        this.setState({ displayValue, clearDisplay: false });
+
+        if(n !== '.') {
+            const i = this.state.current;
+            const newValue = parseFloat(displayValue);
+            const values = [...this.state.values];
+            values[i] = newValue;
+            this.setState({ values });
+            console.log(values)
+        }
     };
     
     render() {
