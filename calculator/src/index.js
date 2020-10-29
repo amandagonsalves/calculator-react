75d408ef0b61f81f381c9a303a36c4fa6bd76e45
Amanda Gonsalves <amandagonsalves75@gmail.com>
2020-10-28 22:24:48 -0300

    update

---
+++ calculator/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Calculator from './main/Calculator';
+import '../src/index.css'
+
+ReactDOM.render(<Calculator />, document.getElementById('root'));
